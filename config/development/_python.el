010645d3e68471e45c8d8717b868f1b438083c93
novg <novg.novg@gmail.com>
2019-10-15 22:52:59 +0700

    Add pyenv

--- config/development/_python.el
+++ config/development/_python.el
@@ -6,6 +6,13 @@
 
 (elpy-enable)
 
+;; Use IPython for REPL
+;; (setq python-shell-interpreter "jupyter"
+      ;; python-shell-interpreter-args "console --simple-prompt"
+      ;; python-shell-prompt-detect-failure-warning nil)
+;; (add-to-list 'python-shell-completion-native-disabled-interpreters
+
+
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
--- config/install-packages/install-packages.el
+++ config/install-packages/install-packages.el
@@ -4,31 +4,45 @@
 
 ;;; Code:
 
+;; Enables basic packaging support
 (require 'package)
 
-(defvar my-packages
-  '(better-defaults
-    powerline
-    helm
-    helm-descbinds
-    elpy
-    flycheck
-    py-autopep8
-    solarized-theme
-    material-theme
-    magit
-    ))
 
+;; Add the Melpa archive to the list of avilable repositories
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;; Initializes tha packages infrasturcture
 (package-initialize)
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+
+;; Installs packages
+;;
+;; my-packages contains a list of package names
+(defvar my-packages
+  '(better-defaults        ;; Set up better Emacs defaults
+    powerline
+    helm                   ;; Emacs incremental completion and selection narrowing framework
+    helm-descbinds         ;; A helm frontend for describe-bindings
+    elpy                   ;; Emasc Lisp Python Environment
+    flycheck               ;; On the fly suntax checker
+    py-autopep8            ;; Run autopep8 on save
+    blacken                ;; Black formatting on save
+    magit                  ;; Git integration
+    solarized-theme        ;; Theme
+    material-theme         ;; Theme
+    ))
+
+;; Scan the list in my-packages
+(mapc #'(lambda (package)
+	  (unless (package-installed-p package)
+	    (package-install package)))
+      my-packages)
+
 
 (provide 'install-packages)
 
