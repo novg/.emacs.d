b699abb74e0b13023b94da174a6e550f9cddcce6
novg <novg.novg@gmail.com>
2024-02-06 17:34:55 +0700

    elisp: Add json modifier

--- elisp/bff.el
+++ elisp/bff.el
@@ -8,3 +8,18 @@
     (replace-regexp "\\\\" "")
     (goto-char (point-min))
     (json-pretty-print (point-min) (point-max))))
+
+(defun my-json->str ()
+  "Convert json to BFF request"
+  (interactive)
+  (progn
+    (search-forward-regexp ":")
+    (forward-char)
+    (insert "\"")
+    (replace-regexp "\"" "\\\\\"")
+    (next-line)
+    (insert "\"")
+    (goto-char (point-min))
+    (replace-regexp  "\{\n+\\\\" "\"{")
+    (goto-char (point-min))
+    (replace-regexp "\n\\| +" "")))
